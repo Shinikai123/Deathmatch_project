885dab8193d635ee6fa78e0530d92f12de8a93f2
Shinikai123 <Amihajlov580@gmail.com>
2022-01-30 19:57:53 +0300

    Revert "experimental"
    This reverts commit f05d13e2ee177c0f12c5a9fc5b0659d6ece455d4.

--- src/store/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import games from './games'
-import visibilityFilter from './visibilityFilter'
-
-export default combineReducers({
-  games,
-  visibilityFilter
-})
